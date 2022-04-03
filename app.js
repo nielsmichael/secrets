32ec73f939ae9d8d7359eadd121b6bbc724eb208
xXnielsXx <nielsmichael@comcast.net>
2022-04-03 14:46:06 +0900

    Insert app.js server boilerplate code

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //jshint esversion:6
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//TO-DO:
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log("Port started on port " + PORT);
+});
